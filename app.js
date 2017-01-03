949c02156077b8251f3432d4343bc4ad4000c0c8
Nyong Otudor <monoandcompany@Nyongs-MacBook-Pro.local>
2017-01-03 14:37:53 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by monoandcompany on 1/3/17.
+ */
+var myApp = angular.module('app', []);
+myApp.controller('MainController', function () {
+
+    this.num1 = 0;
+    this.num2 = 0;
+});
